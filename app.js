6ed54836848c9af13e5f121c3d55275bc4f2136a
nakulsharma15 <nakuls309@gmail.com>
2021-08-30 22:30:10 +0530

    feature: added calculate algorithm in javascript

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 var billAmount = document.querySelector("#bill-amount");
 var cashgiven = document.querySelector("#cash-given");
 var checkButton = document.querySelector("#check-button");
+var message = document.querySelector("#error-message");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
 
-checkButton.addEventListener("click", function checkButtonClicked() {
-    console.log("Clicked");
-})
+var availableNotes = [2000 , 500 , 100 , 20 , 10 , 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    if(billAmount.value > 0) {
+
+        if(cashgiven.value > billAmount.value) {
+            var amountToBeReturned = cashgiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+
+        }
+        else if(cashgiven.value === billAmount.value) {
+            showMessage("No change is to be given")
+        }
+        else{
+            showMessage("Cash given is less than the Bill Amount")
+        }
+
+    }
+    else {
+        showMessage("Invalid Bill Amount");      
+    }
+});
+
+function calculateChange(amountToBeReturned) {
+    for(let i = 0 ; i < availableNotes.length ; i ++) {
+        var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+
+        noOfNotes[i].innerText = numberOfNotes;
+
+    }
+    
+}
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
