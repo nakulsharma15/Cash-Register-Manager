0eb86134181244b8e00d7f69481e623f578b77a0
nakulsharma15 <nakuls309@gmail.com>
2021-08-30 20:02:25 +0530

    Added Cash And bill amount blocks

---
+++ app.js
@@ -0,0 +1,7 @@
+var billAmount = document.querySelector("#bill-amount");
+var cashgiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+
+checkButton.addEventListener("click", function checkButtonClicked() {
+    console.log("Clicked");
+})
